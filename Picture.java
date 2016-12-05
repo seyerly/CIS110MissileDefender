778faef17f3cffb805d691403f21ca559a8081f8
eviebail <ebail@seas.upenn.edu>
2016-12-04 19:41:11 -0500

    Add files via upload

--- Picture.java
+++ Picture.java
@@ -1,3 +1,17 @@
+/*
+ * Partner 1 Name: Evelyn Bailey
+ * Partner 1 Pennkey: ebail
+ * Partner 1 Recitation #: 215
+ * 
+ * Partner 2 Name: Stephen Eyerly
+ * Partner 2 Pennkey: seyerly
+ * Partner 2 Recitation #: 216
+ * 
+ * This class holds the main method for the game of missile defender. It also
+ * contains a method which sets up and stores the background of the game in 
+ * a linked list.
+ */
+
 public class Picture {
     public double x;
     public double y;
@@ -6,6 +20,13 @@ public class Picture {
     public int height;
     public boolean isDeleted = false;
     
+    /*
+     * Name: Picture
+     * Description: constructor for the Picture object to contain background images
+     * Inputs: double xPosition, double yPosition, String imagename, int width, int
+     * height
+     * Outputs: -
+     */
     public Picture (double x, double y, String filename, int width, int height) {
         this.x = x;
         this.y = y;
